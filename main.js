a3c33846fcc3e64de2d8aaf179a67e7329f125ad
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 18:32:04 -0500

    taken code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const GAME_VERSION = "1.0.0";
+const GAME_VERSION = "1";
 
 var versionText = document.getElementById('version');
-versionText.innerText = `Version: ${GAME_VERSION}`;
+versionText.innerText = `v${GAME_VERSION}`;
