c8023e132afeedd75a77528715cbab1eaa0d23c1
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 17:50:53 -0500

    version

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const GAME_STATUS = {
     "version" : "1.0.0",
     "author" : "IdealistCat"
 }
+
+let version_text = document.getElementById('version');
+version_text.innerHTML = GAME_STATUS["version"];
