70a51cb84e059461a862bb962228becf50422f85
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 18:23:47 -0500

    why the fuck wont this work

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const GAME_VERSION = "1.0.0";
 
 var versionText = document.getElementById('version');
-versionText.innerText = `Version: ${GAME_VERSION}`;
+versionText.innerText = 'Version: '+GAME_VERSION;
