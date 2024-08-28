be1f5b8200c79c17d88f99d3b75abb75ee9dec49
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 18:37:58 -0500

    version stuff and credits

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const GAME_VERSION = "1";
+const GAME_VERSION = "1.0.0";
 
 var versionText = document.getElementById('version');
-versionText.innerText = `v${GAME_VERSION}`;
+versionText.innerText = `v. ${GAME_VERSION}`;
