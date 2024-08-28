5d20ac1113c6b986c9a2f0bc40c769ac24f52496
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 18:47:41 -0500

    complicate alot lol

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const GAME_VERSION = "0.1";
-const GAME_STATE = "alpha"
+const GAME_STATE = "alpha";
+const GAME_STATE_VERSION_STRING = GAME_STATE.length > 0 ? `-${GAME_STATE}` : "";
 
 var versionText = document.getElementById('version');
-versionText.innerText = `v. ${GAME_VERSION} ${GAME_STATE.length > 0 ? `-${GAME_STATE}` : ""}`;
+versionText.innerText = `v. ${GAME_VERSION}${GAME_STATE_VERSION_STRING}`;
 
 console.log(fetch('./assets/credits.txt'));
