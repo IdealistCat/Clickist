e6e20766b117a918282ad7c1e59c9392a833ba6d
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 19:00:26 -0500

    1. wow 2. Score Display 3. Autosave?

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// non-gameplay things
+let START_TIME = Date.now();
+
 const GAME_VERSION = "0.1";
 const GAME_STATE = "alpha";
 const GAME_STATE_VERSION_STRING = GAME_STATE.length > 0 ? `-${GAME_STATE}` : "";
@@ -5,4 +8,31 @@ const GAME_STATE_VERSION_STRING = GAME_STATE.length > 0 ? `-${GAME_STATE}` : "";
 var versionText = document.getElementById('version');
 versionText.innerText = `v. ${GAME_VERSION}${GAME_STATE_VERSION_STRING}`;
 
-console.log(fetch('./assets/credits.txt'));
+console.log(fetch('/Clickist/assets/credits.txt'));
+
+// gameplay 
+const GAMEPLAY_TOTAL_CLICKS = 0;
+const GAMEPLAY_MONEY = 0;
+
+const AUTOSAVE_TIME = 5;
+
+let DATE_THING = new Date();
+
+function loadSaveData(data = String) {
+    localStorage.getItem(data);
+}
+function saveData(data = String, content = String) {
+    localStorage.setItem(data, content);
+}
+
+function loadAllData() {
+    GAMEPLAY_TOTAL_CLICKS = loadSaveData('total-clicks');
+    GAMEPLAY_MONEY = loadSaveData('money');
+}
+function saveAllData() {
+    saveData('total-clicks', GAMEPLAY_TOTAL_CLICKS);
+    saveData('money', GAMEPLAY_MONEY);
+    console.log('Saved: ' + START_TIME - Date.now());
+}
+
+setInterval(saveAllData, AUTOSAVE_TIME * 1000);
