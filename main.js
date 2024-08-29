086c31cf09fcd5672360dc0b5668671c572d7c99
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-28 19:16:51 -0500

    texts!

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ versionText.innerText = `v.${GAME_VERSION}${GAME_STATE_VERSION_STRING}`;
 
 console.log(fetch('/Clickist/assets/credits.txt'));
 
-// gameplay 
+// gameplay things
 const GAMEPLAY_TOTAL_CLICKS = 0;
 const GAMEPLAY_MONEY = 0;
 
@@ -19,16 +19,20 @@ const AUTOSAVE_SECOND = 1;
 let DATE_THING = new Date();
 let INTERVAL = setInterval(saveAllData(), AUTOSAVE_TIME * 1000);
 
-function loadSaveData(data = String) {
+var tcText = document.getElementById('total-clicks');
+var moneyText = document.getElementById('money');
+
+function loadSaveData(data = 'money', htmlElement = moneyText) {
     localStorage.getItem(data);
+    htmlElement.innerHTML = localStorage.getItem(data);
 }
 function saveData(data = String, content = String) {
     localStorage.setItem(data, content);
 }
 
 function loadAllData() {
-    GAMEPLAY_TOTAL_CLICKS = loadSaveData('total-clicks');
-    GAMEPLAY_MONEY = loadSaveData('money');
+    GAMEPLAY_TOTAL_CLICKS = loadSaveData('total-clicks', tcText);
+    GAMEPLAY_MONEY = loadSaveData('money', moneyText); // Doesn't require values but are they just in case
 }
 function saveAllData() {
     saveData('total-clicks', GAMEPLAY_TOTAL_CLICKS);
@@ -37,3 +41,5 @@ function saveAllData() {
     console.log('Saved: ' + START_TIME - Date.now());
     clearInterval(INTERVAL);
 }
+
+loadAllData();
