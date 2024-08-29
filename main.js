571ea3d3f5b9c2250af972e091362dab2108dd13
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-29 09:57:42 -0500

    216afee

--- main.js
+++ main.js
@@ -21,33 +21,9 @@ function clickFunction() {
     GAMEPLAY_TOTAL_CLICKS += 1;
     GAMEPLAY_MONEY += GAMEPLAY_MONEY_ADDITION;
 
-    localStorage.setItem('total-clicks', GAMEPLAY_TOTAL_CLICKS);
-    localStorage.setItem('money', GAMEPLAY_MONEY);
-
-    changeTexts();
-}
-function changeTexts() {
     moneyText.innerHTML = `Money: $${GAMEPLAY_MONEY.toString()}`;
-    moneyPer.innerHTML = `Money per Click: ${GAMEPLAY_MONEY_ADDITION}`;
-    tcText.innerHTML = `Total Clicks: ${GAMEPLAY_TOTAL_CLICKS.toString()}`;
+    tcText.innerHTML = `Total Clicks: ${GAMEPLAY_TOTAL_CLICKS.toString()}`;   
 }
 
-function changeMoneyAddition(add = 0) {
-    GAMEPLAY_MONEY_ADDITION += add;
-    changeTexts();
-}
-
-function initalize() {
-    GAMEPLAY_TOTAL_CLICKS = Number(localStorage.getItem('total-clicks'));
-    GAMEPLAY_MONEY = Number(localStorage.getItem('money'));
-    GAMEPLAY_MONEY_ADDITION = Number(localStorage.getItem('money-addition'));
-
-    if (GAMEPLAY_TOTAL_CLICKS < 1) GAMEPLAY_TOTAL_CLICKS = 0;
-    if (GAMEPLAY_MONEY_ADDITION < 1) GAMEPLAY_MONEY_ADDITION = 1;
-    if (GAMEPLAY_MONEY < 1) GAMEPLAY_MONEY = 0;
-
-    changeTexts();
-}
-
+moneyPer.innerHTML = GAMEPLAY_MONEY_ADDITION.toString();
 clickButton.addEventListener("click", clickFunction);
-initalize();
